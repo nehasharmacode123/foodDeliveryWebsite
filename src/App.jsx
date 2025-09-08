9f068a3e1d0ee80c47d60ad429463543d4d4b722
nehasharmacode123 <nehasharma962595@gmail.com>
2025-09-08 12:22:43 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import Navbar from './Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Cart from './pages/cart/Cart'
+import PlaceOrder from './pages/placeOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
 const Home = () => {
+
+  const [category, setCategory] = useState("All");
+
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory}/>
+    </div>
   )
 }
 
